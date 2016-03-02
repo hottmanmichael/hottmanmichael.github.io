99bf97cb3dec7e48fc78d28067a629064c81e970
Michael Hottman <hottmanmichael@gmail.com>
2016-03-02 02:02:38 -0700

    style changes

--- scripts/home.jsx
+++ scripts/home.jsx
@@ -1,31 +1,61 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-var $ = require("jquery");
 
 var Modal = require('./global/modal_window.jsx');
 var ResumeBuilder = require('./global/resume_builder.jsx');
 
 var Home = React.createClass({
 
-   render: function(){
+   translate: {
+      german: 'Deutschsprachiger', //default
+      english: 'German Speaker'
+   },
+
+   getInitialState: function() {
+      return {
+         subtitle: this.translate.german,
+         transCount: 0,
+         countLimit: 20
+      }
+   },
+
+   componentDidMount: function() {
+      setTimeout(this.handleTranslate, 9000)
+   },
+
+   //recursively switches between german & english
+   //slotmachine-like effect, slowing down
+   handleTranslate: function() {
+      if (this.state.transCount < this.state.countLimit) {
+         (this.state.subtitle == this.translate.english) ? this.setState({subtitle: this.translate.german}) : this.setState({subtitle: this.translate.english})
+         this.setState({transCount: this.state.transCount+1});
+         setTimeout(this.handleTranslate, 9000);
+      }
+   },
+
+   /* TODO: Create a function to translate the entire page... Do from app.jsx */
+
+   render: function() {
       return(
-         <div className="main">
+         <div className="main" onScroll={this.handleScroll}>
             <div className="layer-group">
                <div className="layer-base top">
                   <div className='title-box-name'>
-                     <h1 className="title font-large">Michael Hottman</h1>
+                     <p className="title font-page-header">Michael Hottman</p>
+                     <div className="subtitle-box">
+                        <p className="font-center font-small">Web Developer & {this.state.subtitle}</p>
+                     </div>
                   </div>
                </div>
                <div className="layer-upper">
                   <Modal className="open-modal-button" type="button" open={false} label="Open Modal">
-                     <h1 className="font-large">This is an open modal..</h1>
-                     <h3  className="font-small">Not really sure what I'm going to do with it yet, but we'll see... Lots and lots of single line text will go here and it would be really cool if I could scroll left and right on this modal...</h3>
+                     <h3 className="font-small font-center">|___|__ ... __|___|</h3>
                   </Modal>
                </div>
                <div id="resume" className="layer-base resume">
                   <div className='layer-base-inner'>
                      <div className="layer-base-title">
-                        <h1 className="font-large">Resume</h1>
+                        <h1 className="font-large font-center">Resume</h1>
                      </div>
                      <Modal className="open-modal-button resume-item" open={false} label="Item 1">
                         <h1 className="font-large">This is open modal 1..</h1>
--- scripts/test.jsx
+++ scripts/test.jsx
@@ -7,24 +7,20 @@ var Parallax = React.createClass({
       return(
          <div className="main">
             <div className="parallax">
-               <div id="group2" className="parallax__group">
+               <div id="group1" className="parallax__group">
+                  <div className="parallax__layer parallax__layer--deep">
+                     <h1 className="title">Deep Layer</h1>
+                  </div>
                   <div className="parallax__layer parallax__layer--base">
                      <h1 className="title">Base Layer</h1>
                   </div>
-                  <div className="parallax__layer parallax__layer--back">
-                     <h1 className="title">Back Layer</h1>
-                  </div>
+               </div>
+               <div id="group2" className="parallax__group">
                   <div className="parallax__layer parallax__layer--deep">
                      <h1 className="title">Deep Layer</h1>
                   </div>
                </div>
                <div id="group3" className="parallax__group">
-                  <div className="parallax__layer parallax__layer--base">
-                     <h1 className="title">Base Layer</h1>
-                  </div>
-                  <div className="parallax__layer parallax__layer--back">
-                     <h1 className="title">Back Layer</h1>
-                  </div>
                   <div className="parallax__layer parallax__layer--deep">
                      <h1 className="title">Deep Layer</h1>
                   </div>
@@ -36,3 +32,11 @@ var Parallax = React.createClass({
 })
 
 module.exports = Parallax;
+
+
+// <div className="parallax__layer parallax__layer--base">
+//    <h1 className="title">Base Layer</h1>
+// </div>
+// <div className="parallax__layer parallax__layer--back">
+//    <h1 className="title">Back Layer</h1>
+// </div>
